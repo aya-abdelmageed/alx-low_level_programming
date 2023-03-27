b44ad5145bad2c524d9c3fad6b50cb1223fa4ff5
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-03-27 15:37:36 -0700

    advanced

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer
+ * return: integer
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1, num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else
+			break;
+	} while (*s++);
+	return (num * sign);
+}
