6e960d06b68afd8f3d1e3f790c79b8cf0c4d4b79
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-03-30 12:37:55 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,18 @@
 
 char *leet(char *s)
 {
-	int i = 0;
+	int i = 0, j;
+	char a[] = "aeotlAEOTL";
+	char n[] = "4307143071";
 
-	for (s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j <= 9; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = n[j];
+		}
+		i++;
 	}
 	return (s);
 }
