e4f30f3f3b302b754f7febec6629afd920daee62
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-03-30 13:41:29 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,16 +8,20 @@
 
 char *rot13(char *s)
 {
-	int i = 0, j;
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j;
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
 		for (j = 0; j <= 51; j++)
 		{
-			if (s[i] == a[j])
-				s[i] = n[j];
+			if (*(s + i) == a[j])
+			{
+				*(s + i) = rot[j];
+				break;
+			}
 		}
 		i++;
 	}
