33ca06ace8c5309f71a8b401892fd58c3b1eaa4f
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-03-30 12:02:21 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,14 +2,15 @@
 
 /**
  * cap_string - function that capitalizes all words of a string
- * @str: string 
+ * @str: string
  * Return: string
  */
 
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i = 0, j;
 	bool flag = 1;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	while (*(str + i) != '\0')
 	{
@@ -23,8 +24,14 @@ char *cap_string(char *str)
 		}
 		else
 		{
-			if (*(str + i) < 'a' && *(str + i) > 'z' && *(str + i) < 'A' && *(str + i) >'Z')
-				flag = 1;
+			for (j = 0; j < 13; j++)
+			{
+				if (*(str + i) == a[j])
+				{
+					flag = 0;
+					break;
+				}
+			}
 		}
 		i++;
 	}
