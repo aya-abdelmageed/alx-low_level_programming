19ce86b7aaf527954f62dc71a8f4bd1ee5e9d30f
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-04-17 09:01:23 -0700

    task 0

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: struct dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
