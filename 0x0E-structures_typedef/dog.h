ef52b5887e2def50c0dfe36719a77d77d743ff33
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-04-17 08:24:53 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog attributes
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: dog attributes
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
