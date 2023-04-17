f8943bdf08c6b8d591ecbbd28491bae7b9932b32
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-04-17 08:35:14 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,16 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+/**
+ * dog_t - typedef for dog struct
+ */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
