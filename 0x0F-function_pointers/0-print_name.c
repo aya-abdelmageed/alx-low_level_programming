88da1ba566f87f9db13a2ea7b203cffc3e44ac90
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-04-19 09:10:46 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: input
+ * @f: pointer to function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
