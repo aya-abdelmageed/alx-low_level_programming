db4f6d6b8f07bab63dd74dd0a55ed0fead9fc228
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-04-27 10:03:49 -0700

    add task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: struct node
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *head = h;
+	size_t count = 0;
+
+	while (head != NULL)
+	{
+		if (head->str != NULL)
+			printf("[%d] %s\n", head->len, head->str);
+		else
+			printf("[%d] %s\n", 0, "(nil)");
+		count++;
+		head= head->next;
+	}
+	return (count);
+}
