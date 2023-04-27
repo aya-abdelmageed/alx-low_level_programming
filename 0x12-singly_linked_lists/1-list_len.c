c0b6b0715614bf9be860789d1e4f1fd9c3f81ea0
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-04-27 10:16:41 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,2 +1,21 @@
 #include "lists.h"
 
+/**
+ * list_len - returns the number of elements in a linked list_t
+ * @h: list pointer
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
+
+
