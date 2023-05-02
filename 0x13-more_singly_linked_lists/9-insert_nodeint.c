2d15a80570c6f97cacb0677b22b130b6bc1e195f
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-05-02 10:53:56 -0700

    fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	h = *head;
 	if (idx != 0)
 	{
-		for ( ; i < idx && h != NULL; i++)
+		for ( ; i < idx - 1 && h != NULL; i++)
 			h = h->next;
 	}
 
