6558fa964cb58fc2c54e66ee2af9d36e01bf2edc
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-05-11 14:40:44 -0700

    fix 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600)
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 
@@ -28,5 +28,5 @@ int create_file(const char *filename, char *text_content)
 
 	write(file, text_content, i);
 	close(file);
-		return (1);
+	return (1);
 }
