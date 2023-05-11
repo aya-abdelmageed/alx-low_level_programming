cf5c037b23f08901df8925c0b89b09ea1f9fd8cd
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-05-11 15:01:56 -0700

    fix 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
 
