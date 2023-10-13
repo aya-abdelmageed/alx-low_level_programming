87e0cc605c983d0fa88ad49fddeef6ab5df3ab36
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-10-13 13:21:44 -0700

    add 9

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - func that searches for a value in an array
+ *
+ * @array: array
+ * @size: size of the array
+ * @value: value we being search for
+ * Return: index
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t lo, hi, pos;
+
+	if (array == NULL)
+		return (-1);
+
+	lo = 0;
+	hi = size - 1;
+
+	while (lo <= hi && value >= array[lo] && value <= array[hi])
+	{
+		pos = lo + (size_t)(((double)(hi - lo) / (array[hi] - array[lo])) *
+				(value - array[lo]));
+
+		printf("Value checked array[%d]", (int)pos);
+
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
+		else
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			lo = pos + 1;
+		else
+			hi = pos - 1;
+		if (lo == hi)
+			break;
+	}
+	return (-1);
+}
